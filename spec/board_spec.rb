bee7d88b9bee0770302f7fe40ffe852cf2b73a76
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-01-31 22:49:42 +0500

    test: add failing test for render_view method

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,30 @@
-require_relative "../lib/connect_four/board"
+require_relative '../lib/connect_four/board'
 
 describe Board do
-  it "Has Board Class" do
-    board = Board.new
+  let(:board) { Board.new }
+  it 'Has Board Class' do
     expect(board).to be_kind_of(Board)
   end
+  it 'Render View' do
+    board_layout = [
+      %w[a1 a2 a3 a4 a5 a6 a7],
+      %w[b1 b2 b3 b4 b5 b6 b7],
+      %w[c1 c2 c3 c4 c5 c6 c7],
+      %w[d1 d2 d3 d4 d5 d6 d7],
+      %w[e1 e2 e3 e4 e5 e6 e7],
+      %w[f1 f2 f3 f4 f5 f6 f7]
+    ]
+    result = board.render_view
+    expect(result).to eq(board_layout)
+  end
 end
+
+# 6 rows and 7 column
+# a 1 2 3 4 5 6 7
+# b
+# C
+# d
+# e
+# f
+#
+#
