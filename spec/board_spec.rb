e73f194dfe7ee0e1ce862c5a3c828e1e77ad8265
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-02-15 20:08:11 +0500

    test: remove unnecessary instance variable from false condition testing of row and columns

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -45,14 +45,6 @@ describe Board do # rubocop:disable Metrics/BlockLength
       expect(result).to be true
     end
     it 'return false when four are not connect horizontally' do
-      board.instance_variable_set(:@board_layout, [
-                                    %w[a1 a2 a3 a4 a5 a6 a7],
-                                    %w[b1 b2 b3 b4 b5 b6 b7],
-                                    %w[c1 c2 c3 c4 c5 c6 c7],
-                                    %w[d1 d2 d3 d4 d5 d6 d7],
-                                    %w[e1 e2 e3 e4 e5 e6 e7],
-                                    %w[f1 f2 f3 f4 f5 f6 f7]
-                                  ])
       result = board.check_horizontal_connection
       expect(result).to be false
     end
@@ -71,14 +63,6 @@ describe Board do # rubocop:disable Metrics/BlockLength
       expect(result).to be true
     end
     it 'return false when four are not connected vertically' do
-      board.instance_variable_set(:@board_layout, [
-                                    %w[a1 a2 a3 a4 a5 a6 a7],
-                                    %w[b1 b2 b3 b4 b5 b6 b7],
-                                    %w[c1 c2 c3 c4 c5 c6 c7],
-                                    %w[d1 d2 d3 d4 d5 d6 d7],
-                                    %w[e1 e2 e3 e4 e5 e6 e7],
-                                    %w[f1 f2 f3 f4 f5 f6 f7]
-                                  ])
       result = board.check_vertical_connection
       expect(result).to be false
     end
