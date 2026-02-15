b8c41b0b4ebdb5b3f34f17725268d287f03741ce
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-02-15 20:43:32 +0500

    test: add failing test for method to simulate gravity

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -104,4 +104,19 @@ describe Board do # rubocop:disable Metrics/BlockLength
       expect(result).to be false
     end
   end
+  context 'gravity' do
+    it 'it pulls the token downward if made at above spots' do
+      expected_board_layout = [
+        %w[a1 a2 a3 a4 a5 a6 a7],
+        %w[b1 b2 b3 b4 b5 b6 b7],
+        %w[c1 c2 c3 c4 c5 c6 c7],
+        %w[d1 d2 d3 d4 d5 d6 d7],
+        %w[e1 e2 e3 e4 e5 e6 e7],
+        %w[🟡 f2 f3 f4 f5 f6 f7]
+      ]
+      board.change_view('a1', '🟡')
+      result = board.apply_gravity
+      expect(result).to eq(expected_board_layout)
+    end
+  end
 end
