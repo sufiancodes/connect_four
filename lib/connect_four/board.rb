2769a56a2918f3f45c8efa7252e512dba22f73af
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-02-18 20:47:26 +0500

    feat: add the new design of class with constant and initialize method

--- lib/connect_four/board.rb
+++ lib/connect_four/board.rb
@@ -4,14 +4,16 @@ class Board # rubocop:disable Style/Documentation
   attr_accessor :board_layout
 
   EMPTY_TOKEN = "\u25ef"
-  WHITE_TOKEN = "\u25cf"
-  BLACK_TOKEN = "\u26ab"
+  WHITE_TOKEN = "\e[37m\u23FA\e[0m"
+  RED_TOKEN = "\e[31m\u23FA\e[0m"
 
   NUMBER_OF_ROWS = 6
   NUMBER_OF_COLUMN = 7
+
+  def initialize
+    @board_layout = Array.new(6){Array.new(7){EMPTY_TOKEN}}
+  end
 end
 
 board = Board.new
-
-puts "\u22C5"
-puts "\u25CF"
+pp board.board_layout
