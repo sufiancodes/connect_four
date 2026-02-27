e0f7edaf12860667f9a455fe4e68c54c3cdec762
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-02-27 20:20:51 +0500

    test: add failing test for method to check if game is over

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,12 @@ require_relative '../lib/connect_four/game'
 
 describe Game do
   let(:game) {Game.new}
+  let(:board) {instance_double 'Board'}
   it 'Has the Game class' do
     expect(game).to be_kind_of(Game)
+  end
+  it 'return true when game is over' do
+    allow(board).to receive(:four_in_row?).and_return(true)
+    expect(game.over?).to eq(true)
   end
 end
