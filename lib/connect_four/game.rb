c46f8b6ac192e3715f4deb008d996e8041295f76
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-02-27 20:58:23 +0500

    feat: add method to check if game is over along with initialize method

--- lib/connect_four/game.rb
+++ lib/connect_four/game.rb
@@ -1,3 +1,15 @@
+require_relative 'board'
 class Game
-  
+  def initialize(board = Board.new)
+    @current_player = nil
+    @board = board
+  end
+
+  def over?
+    if @board.four_in_row? || @board.four_in_column? || @board.primary_diagonal? || @board.secondary_diagonal?
+      return true
+    end
+
+    false
+  end
 end
