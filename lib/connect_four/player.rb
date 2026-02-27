72864a44c4e971a40c858cd0f224e5d6fa136eaa
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-02-27 17:22:54 +0500

    feat: add welcome method for promoting and storing names of users in start

--- lib/connect_four/player.rb
+++ lib/connect_four/player.rb
@@ -1,5 +1,15 @@
-module Player
-  def self.welcome
-    "Welcome\nPerson to go first is Player_one(Red) and person to go second is Player_two(Yellow)"
+class Player
+  attr_accessor :first_player, :second_player
+
+  def initialize
+    @first_player = nil
+    @second_player = nil
+  end
+
+  def welcome
+    puts 'Enter the name of first player'
+    self.first_player = gets.chomp
+    puts 'Enter the name of second player'
+    self.second_player= gets.chomp
   end
 end
