372517cd6f629a31fed21674a88c22f8ff4eacee
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-02-27 17:19:48 +0500

    test: add failing tests for promoting and storing players names

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,15 @@
-require_relative "../lib/connect_four/player"
+require_relative '../lib/connect_four/player'
 
 describe Player do
-  it "Welcome the players" do
-    expect(Player.welcome).to eq("Welcome\nPerson to go first is Player_one(Red) and person to go second is Player_two(Yellow)")
+  let(:player) { Player.new }
+  it 'Prompts first user for their name and save it' do
+    expect(player).to(receive(:gets).and_return('Alex', 'Alexa'))
+    expect { player.welcome }.to output(/Enter the name of first player/).to_stdout
+    expect(player.first_player).to eq('Alex')
+  end
+  it 'Prompts second user for their name and save it' do
+    expect(player).to(receive(:gets).and_return('Alex', 'Alexa'))
+    expect { player.welcome }.to output(/Enter the name of second player/).to_stdout
+    expect(player.second_player).to eq('Alexa')
   end
 end
