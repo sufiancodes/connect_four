0f0153f5f8bf69ba7b4293312c6c1b771a5778dc
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-02-28 20:25:07 +0500

    test: add tests to over? method for when primary_diagonal or secondary_diagonal return true

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -17,5 +17,18 @@ describe Game do
       allow(board).to receive(:four_in_column?).and_return(true)
       expect(game.over?).to eq(true)
     end
+    it 'return true when match in primary diagonal' do
+      allow(board).to receive(:four_in_row?).and_return(false)
+      allow(board).to receive(:four_in_column?).and_return(false)
+      allow(board).to receive(:primary_diagonal?).and_return(true)
+      expect(game.over?).to eq(true)
+    end
+    it 'return true when match in secondary diagonal' do
+      allow(board).to receive(:four_in_row?).and_return(false)
+      allow(board).to receive(:four_in_column?).and_return(false)
+      allow(board).to receive(:primary_diagonal?).and_return(false)
+      allow(board).to receive(:primary_diagonal?).and_return(true)
+      expect(game.over?).to eq(true)
+    end
   end
 end
