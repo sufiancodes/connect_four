d72315181b0cd3134d9441ee0f568e14daa383a1
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-03-02 20:25:32 +0500

    feat: add way to call welcome method in game

--- lib/connect_four/game.rb
+++ lib/connect_four/game.rb
@@ -3,11 +3,13 @@
 require_relative 'board'
 require_relative 'player'
 class Game
-  attr_reader :board
+  attr_accessor :board, :moves
 
   def initialize(board = Board.new)
     @current_player = nil
     @board = board
+    @moves = 40
+    @player = Player.new
   end
 
   def over?
@@ -17,6 +19,37 @@ class Game
 
     false
   end
+
+  def welcome
+    @player.welcome
+  end
 end
 
 game = Game.new
+
+# while game.over? != true && game.moves != 42
+#   game.welcome
+#   puts game.board
+#   puts 'White or Red'
+#   token = gets.chomp
+#   puts 'Enter Column to drop'
+#   column = gets.chomp.to_i
+#   if token == 'white'
+#     game.board.drop_token(column, Board::WHITE_TOKEN)
+#     game.moves += 1
+#   else
+#     game.board.drop_token(column, Board::RED_TOKEN)
+#     game.moves += 1
+
+#   end
+#   puts game.board
+# end
+
+# At 42 moves its supposed to be draw
+# until game.over?
+#   print_board
+#   prompt_input
+#   drop_token
+# end
+
+# display_end_result
