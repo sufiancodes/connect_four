ef42a51601e494bc883f5a447d848c94343f0d26
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-03-03 20:36:23 +0500

    feat: add method to give the current player depending turn and method to switch player

--- lib/connect_four/player.rb
+++ lib/connect_four/player.rb
@@ -1,15 +1,28 @@
 class Player
-  attr_accessor :first_player, :second_player
+  attr_accessor :first_player, :second_player, :turn
 
   def initialize
     @first_player = nil
     @second_player = nil
+    @turn = 0
   end
 
   def welcome
     puts 'Enter the name of first player'
     self.first_player = gets.chomp
     puts 'Enter the name of second player'
-    self.second_player= gets.chomp
+    self.second_player = gets.chomp
+  end
+
+  def current_name
+    if turn.zero?
+      first_player
+    else
+      second_player
+    end
+  end
+
+  def switch!
+    self.turn = (turn.zero? ? 1 : 0)
   end
 end
