78dac9f90d8be2f5e7f6805446bd1ac24be7d3d2
Sufian <176370395+sufiancodes@users.noreply.github.com>
2026-03-03 20:33:25 +0500

    test: add tests for method to give current player depending on the turn

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -12,4 +12,17 @@ describe Player do
     expect { player.welcome }.to output(/Enter the name of second player/).to_stdout
     expect(player.second_player).to eq('Alexa')
   end
+  it 'returns first player name when turn is 0 (current_name)' do
+    player.first_player = 'Alex'
+    player.second_player = 'Alexa'
+    player.turn = 0
+    expect(player.current_name).to eq('Alex')
+  end
+
+  it 'returns second player name when turn is 1 (current_name)' do
+    player.first_player = 'Alex'
+    player.second_player = 'Alexa'
+    player.turn = 1
+    expect(player.current_name).to eq('Alexa')
+  end
 end
